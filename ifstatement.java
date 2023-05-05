0aaf0d842ecb58413ebca593a5e1539d64e34360
MGaonkar1999 <mvgaonkar1999@gmail.com>
2023-05-05 12:01:22 +0530

    new branch

---
+++ ifstatement.java
@@ -0,0 +1,12 @@
+public class ifstatement
+{
+public static void main(String[] args) 
+{
+int var=10;
+if(var%2==0)
+{
+System.out.println("number is even");
+System.out.println(" *******");
+}
+}
+}
